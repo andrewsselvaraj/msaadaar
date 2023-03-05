6cb8b62eabc7846046d5d7c33ab8d350295db958
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-05 22:48:44 +0530

    Base Versoin

---
+++ src/main/java/com/msaadhaar/MsaadaarApplication.java
@@ -0,0 +1,13 @@
+package com.msaadhaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaadaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaadaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msaadhaar/MsaadaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msaadhaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaadaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
